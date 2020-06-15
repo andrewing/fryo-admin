e096b21989167d7e8e6fffd654aab047f6f09eb1
andrewing <john_andrew_ing@dlsu.edu.ph>
2020-06-15 21:06:26 +0800

    feat: initial commit

---
+++ config/database.js
@@ -0,0 +1,19 @@
+module.exports = ({ env }) => ({
+  defaultConnection: 'default',
+  connections: {
+    default: {
+      connector: 'mongoose',
+      settings: {
+        host: env('DATABASE_HOST', '127.0.0.1'),
+        srv: env.bool('DATABASE_SRV', true),
+        port: env.int('DATABASE_PORT', 27017),
+        database: env('DATABASE_NAME', 'fryo-admin'),
+        username: env('DATABASE_USERNAME', ''),
+        password: env('DATABASE_PASSWORD', ''),
+      },
+      options: {
+        ssl: env.bool('DATABASE_SSL', false),
+      },
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '52a4fed2-0295-43ce-9758-5ce188c1933c'
+};

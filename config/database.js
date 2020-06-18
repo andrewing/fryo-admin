23534222bb8e80263e1e5abee6f6babd6bf7da76
andrewing <john_andrew_ing@dlsu.edu.ph>
2020-06-18 11:22:51 +0800

    feat: added route

--- config/database.js
+++ config/database.js
@@ -5,7 +5,7 @@ module.exports = ({ env }) => ({
       connector: 'mongoose',
       settings: {
         host: env('DATABASE_HOST', '127.0.0.1'),
-        srv: env.bool('DATABASE_SRV', true),
+        srv: env.bool('DATABASE_SRV', false),
         port: env.int('DATABASE_PORT', 27017),
         database: env('DATABASE_NAME', 'fryo-admin'),
         username: env('DATABASE_USERNAME', ''),
